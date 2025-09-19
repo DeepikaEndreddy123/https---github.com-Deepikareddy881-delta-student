0d9f72e414f9382da0e7673d34cd7d9dcfcb49bb
DeepikaEndreddy123 <deepikaendreddy881@gmail.com>
2025-09-19 20:00:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser()); 
 app.use((req, res, next) => {
-  res.locals.sucess = req.flash("sucess");
+  res.locals.sucess = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
   next();
